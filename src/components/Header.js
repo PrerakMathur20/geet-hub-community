68754fbafde155eed6d26d6cabb876192e3f7094
Palash Baderia <palash.baderia@outlook.com>
2022-11-14 23:40:01 +0530

    new veri.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMoon } from "@fortawesome/sharp-solid-svg-icons";
-import { faSun } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
 
@@ -31,7 +29,7 @@ const Header = () => {
           <a href="https://geethub.netlify.app">Geet-Hub</a>
           <div className="right menu">
             <div className="item">
-            <a href="#" className="btn" onClick={() => toggleTheme()}>{state ? <FontAwesomeIcon size="xl" icon={faSun}  /> : <FontAwesomeIcon size="xl" icon={faMoon} />}</a>
+            {/* <a href="#" className="btn" onClick={() => toggleTheme()}>{state ? <FontAwesomeIcon size="xl" icon={faSun}  /> : <FontAwesomeIcon size="xl" icon={faMoon} />}</a> */}
               <a
                 href="https://github.com/PrerakMathur20/geet-hub-community"
                 className="item"
